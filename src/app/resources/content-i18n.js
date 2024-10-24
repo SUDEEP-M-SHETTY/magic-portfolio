68489d0c158264f48955a0c67a8814915fd2ee2e
SUDEEP M SHETTY <86517389+SUDEEP-M-SHETTY@users.noreply.github.com>
2024-10-24 10:15:09 +0000

    first commit

--- src/app/resources/content-i18n.js
+++ src/app/resources/content-i18n.js
@@ -2,15 +2,15 @@ import { InlineCode } from "@/once-ui/components";
 
 const createI18nContent = (t) => {
     const person = {
-        firstName: 'Selene',
+        firstName: 'Aayush',
         lastName:  'Yu',
         get name() {
             return `${this.firstName} ${this.lastName}`;
         },
         role:      t("person.role"),
         avatar:    '/images/avatar.jpg',
-        location:  'Asia/Jakarta',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-        languages: ['English', 'Bahasa']  // optional: Leave the array empty if you don't want to display languages
+        location:  'Asia/India',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+        languages: ['English', 'Hindi', 'Kannada']  // optional: Leave the array empty if you don't want to display languages
     }
 
     const newsletter = {
